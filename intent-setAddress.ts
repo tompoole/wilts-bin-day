df2c24169f9960a234ac3e617da8b66c21595a6e
Tom Poole <tom.poole@just-eat.com>
2017-05-28 23:06:08 +0100

    Update set address intent.

--- intent-setAddress.ts
+++ intent-setAddress.ts
@@ -21,7 +21,7 @@ export class SetAddressIntent implements Intent {
         this._alexaApi = alexaApi;
     }
 
-    getSlot(alexa: Handler, slot: string) {
+    getSlotValue(alexa: Handler, slot: string) {
         try {
             let slotsObj = (alexa.event.request as any).intent.slots;
             return slotsObj[slot].value;
@@ -33,11 +33,23 @@ export class SetAddressIntent implements Intent {
         }
     }
 
+    isSlotConfirmed(alexa: Handler, slot: string):boolean {
+        try {
+            let slotsObj = (alexa.event.request as any).intent.slots;
+            return slotsObj[slot].confirmationStatus == "CONFIRMED";
+        }
+        catch(e)
+        {
+            console.error("Error getting slot value ", e);
+            return false;
+        }
+    }
+
     public async handler(alexa: Handler) {
         let addressData : AddressResponse, matchingAddreses: Address[];
         
         try {
-            addressData = await this._alexaApi.getAddressForDevice(alexa);
+            addressData = await this._alexaApi.getAddressForDevice(alexa.event);
         }
         catch(e) {
             // alexa.emit(":tell", responses.ErrorGettingAddressData );
@@ -58,33 +70,38 @@ export class SetAddressIntent implements Intent {
         // alexa.state.postcode = addressData.postcode;
         // console.log("state ", alexa.state.postcode);
         
-        let houseNumber = this.getSlot(alexa, "HouseNumber");
+        let houseNumber = this.getSlotValue(alexa, "HouseNumber");
         console.log("housenumber", houseNumber);
 
         if (!houseNumber) {
 
-            if (!addressData.addressLine1) {
                 console.log("getting slot...")
-                alexa.emit(":elicitSlot", "HouseNumber", "Okay, I think your postcode is " + addressData.postcode + ". What is your house number?" );
+                alexa.emit(":elicitSlot", "HouseNumber", "Okay, I think your postcode is " + addressData.postcode + ". What is your house number?", "Sorry, can you say that again?");
+                console.log("Send ElicitSlot response for HouseNumber")
                 return;
-            }
         }
         else {
 
-            try {
-                matchingAddreses = await this._addressService.getAddresses(addressData.postcode, houseNumber);
-
-                if (matchingAddreses.length > 1) {
-                    alexa.emit(":tell", "Multiple addresses found at this address. You'll need to setup ")
-                }
-                else {
-                    
-                }
-
+            if (this.isSlotConfirmed(alexa, "HouseNumber")) {
+                alexa.emit(":tell", "Great! You're all setup.");
             }
-            catch(e) {
-                alexa.emit(":tell", responses.ErrorFindingAddress);
-                return;
+            else {
+                try {
+                    matchingAddreses = await this._addressService.getAddresses(addressData.postcode, houseNumber);
+
+                    if (matchingAddreses.length > 1) {
+                        alexa.emit(":tell", "Multiple addresses found matching your postcode and house number. You'll need to setup your address in the Alexa App.")
+                    }
+                    else {
+                        let address = matchingAddreses[0];
+                        alexa.emit(":confirmSlot", "HouseNumber", "Okay, got it. I think your address is " + address.address + ". Is this correct?")
+                    }
+
+                }
+                catch(e) {
+                    alexa.emit(":tell", responses.ErrorFindingAddress);
+                    return;
+                }
             }
         }
     }
